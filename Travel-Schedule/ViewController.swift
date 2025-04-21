12da3685ba668b3cc6614072f9eb1f24b116f3db
Aleksandr <Al3ksandr213@yandex.ru>
2025-04-21 18:24:21 +0300

    Initial Commit

---
+++ Travel-Schedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel-Schedule
+//
+//  Created by 1111 on 21.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
