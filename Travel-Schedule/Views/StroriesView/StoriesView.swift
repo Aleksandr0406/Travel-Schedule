d4db549be1be4db6c8e0177427c9fb448cea37c1
Aleksandr <Al3ksandr213@yandex.ru>
2025-05-28 14:38:51 +0300

    feat: реализовать функционал просмотра сторис

---
+++ Travel-Schedule/Views/StroriesView/StoriesView.swift
@@ -0,0 +1,102 @@
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    struct Configuration {
+        let timerTickInternal: TimeInterval
+        let progressPerTick: CGFloat
+        
+        init(
+            storiesCount: Int,
+            secondsPerStory: TimeInterval = 5,
+            timerTickInternal: TimeInterval = 0.25
+        ) {
+            self.timerTickInternal = timerTickInternal
+            self.progressPerTick = 1.0 / CGFloat(storiesCount) / secondsPerStory * timerTickInternal
+        }
+    }
+    
+    private let stories: [Story]
+    private let configuration: Configuration
+    private var currentStory: Story { stories[currentStoryIndex] }
+    private var currentStoryIndex: Int { max(index, Int(progress * CGFloat(stories.count))) }
+    
+    @State private var index: Int
+    @State private var progress: CGFloat
+    @State private var timer: Timer.TimerPublisher
+    @State private var cancellable: Cancellable?
+    @Binding var stateProperty: StateProperties
+    
+    init(
+        stories: [Story] = [.story1, .story2, .story3],
+        stateProperty: Binding<StateProperties>,
+        index: Int
+    ) {
+        self.stories = stories
+        configuration = Configuration(storiesCount: stories.count)
+        timer = Self.createTimer(configuration: configuration)
+        _stateProperty = stateProperty
+        self.index = index
+        self.progress = CGFloat(index)/CGFloat(stories.count)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            StoryView(story: currentStory)
+            ProgressBar(numberOfSections: stories.count, progress: progress)
+                .padding(.init(top: 28, leading: 12, bottom: 12, trailing: 12))
+            CloseButton(action: { stateProperty.isPresentingStory = false })
+                .padding(.top, 57)
+                .padding(.trailing, 12)
+        }
+        .onAppear {
+            timer = Self.createTimer(configuration: configuration)
+            cancellable = timer.connect()
+        }
+        .onDisappear {
+            cancellable?.cancel()
+        }
+        .onReceive(timer) { _ in
+            timerTick()
+        }
+        .onTapGesture {
+            nextStory()
+            resetTimer()
+        }
+    }
+    
+    private func timerTick() {
+        var nextProgress = progress + configuration.progressPerTick
+        if nextProgress >= 1 {
+            nextProgress = 0
+            index = 0
+        }
+        withAnimation {
+            progress = nextProgress
+        }
+    }
+    
+    private func nextStory() {
+        let storiesCount = stories.count
+        let currentStoryIndex = Int(progress * CGFloat(storiesCount))
+        let nextStoryIndex = currentStoryIndex + 1 < storiesCount ? currentStoryIndex + 1 : 0
+        withAnimation {
+            index = 0
+            progress = CGFloat(nextStoryIndex) / CGFloat(storiesCount)
+        }
+    }
+    
+    private func resetTimer() {
+        cancellable?.cancel()
+        timer = Self.createTimer(configuration: configuration)
+        cancellable = timer.connect()
+    }
+    
+    private static func createTimer(configuration: Configuration) -> Timer.TimerPublisher {
+        Timer.publish(every: configuration.timerTickInternal, on: .main, in: .common)
+    }
+}
+
+//#Preview {
+//    ContentView(stateProperty: .constant(StateProperties()), index: 0)
+//}
---
+++ Travel-Schedule/Views/StroriesView/Story.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct Story {
+    let backgroundColor: Color
+    let title: String
+    let description: String
+
+    static let story1 = Story(
+        backgroundColor: .story1Background,
+        title: "🎉 ⭐️ ❤️",
+        description: "Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 "
+    )
+
+    static let story2 = Story(
+        backgroundColor: .story2Background,
+        title: "😍 🌸 🥬",
+        description: "Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 "
+    )
+
+    static let story3 = Story(
+        backgroundColor: .story3Background,
+        title: "🧀 🥑 🥚",
+        description: "Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 "
+    )
+}
